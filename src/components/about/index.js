677e7cf231d27d8bfff4bd46e9f9d6fb85a7eaef
ah-lai <lai.alvin189@gmail.com>
2020-11-15 23:32:18 -0500

    about me text

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -6,12 +6,16 @@ class About extends React.Component {
         return(
             <section>
                 <div id="about" class={styles.condiv}>
-                    <h1 className={styles.name}>Hi! I'm Alvin Lai</h1>
-                    <h3>Full Stack Web <u>Developer</u></h3>
+                    <h1 className={styles.name}>Hi and welcome!</h1>
                     <br></br>
                     <p className={styles.about}>
-                        I am currently a engineering student at the University of Waterloo with a major in Mechanical Engineering and an option in 
-                        Artificial Intelligence. I love learning about new technology and building new things! XXXXXXXXXXXXXXXX continue writing more l
+                        I am currently an engineering student at the University of Waterloo with an option in Artificial Intelligence. I'm passionate and always eager to learn new technology!  
+                        <br></br>
+                        <br></br>
+                        I have strong problem solving skills and diligent in self-learning; leading to me achieve Dean's List for multiple semsters and an overall GPA of 3.9/4. 
+                        As well, I have proven communication and leadership skills shown by obtaining Duke of Edinburgh Award and Toastmasters International Certification!
+                        <br></br>
+                        <br></br>
                     </p>
                 </div>
             </section>
